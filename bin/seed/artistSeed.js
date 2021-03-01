b94b4b646df933d6c8aea1fad9b9db7a9cb1eea5
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-03-01 12:20:29 +0100

    with images

--- bin/seed/artistSeed.js
+++ bin/seed/artistSeed.js
@@ -7,25 +7,25 @@ const artists = [
         name : "The Hu",
         isBand : true,
         description : "The HU is a band from Mongolia that blends heavy metal and traditional Mongolian throat singing.",
-        picture : "https://m.media-amazon.com/images/I/71-9I4-jfwL._SS500_.jpg"
+        picture : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597294/Artistify/TheHu_li5cyi.jpg"
     },
     {
         name : "Aurelio Voltaire",
         isBand : true,
         description : "Musician, filmmaker, author and gothic horror personality.",
-        picture : "https://images-na.ssl-images-amazon.com/images/I/61GxA0ZoWML.jpg"
+        picture : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597295/Artistify/Voltaire_ogn8ek.jpg"
     },
     {
         name : "The Correspondents",
         isBand : true,
         description : "The Correspondents were a British electro swing duo based in London, formed by vocalist and MC Mr. Bruce and producer Tim Cole in 2007.",
-        picture : "https://lh3.googleusercontent.com/proxy/QJ01MdpkQlg4-Vvi0d39_MrfeEMjyqos1X0QrFpaMb9Sgu8oUCAR5LVOBTF6_5smrHDwwddRcGLJBbW_sDuuM26CgMVhytA-kFfCDOx4RGqEY4-pq8vnnTU_fFMkIiC4WK2yBxpH-L_SUxG1X0xXRCvo4yhPj8Jta2cY1Wo"
+        picture : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597294/Artistify/Correspondents_u0m7he.jpg"
     },
     {
         name : "AURORA",
         isBand : false,
         description : "Aurora Aksnes, known mononymously as AURORA, is a Norwegian singer, songwriter, and record producer.",
-        picture : "https://e.snmc.io/i/600/w/1b5204f491b4c2da82ec15ed3da31dba/5935347/aurora-allmydemonsgreetingmeasafriend-cover-art.jpg"
+        picture : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597295/Artistify/Aurora_kicqwq.webp"
         },
 ]
 
--- bin/seed/labelSeed.js
+++ bin/seed/labelSeed.js
@@ -8,7 +8,7 @@ const label = [{
     street : "Codrington Mews, Notting Hill"
     streetNumber : 1
     zipcode : "W11 2EH"
-    logo : 
+    logo : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597294/Artistify/XL_ladgfw.webp"
 }, 
 
 {
@@ -18,7 +18,7 @@ const label = [{
     street : "PO Box"
     streetNumber : 379
     zipcode : "10276"
-    logo : 
+    logo : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597295/Artistify/Ghostly_btlkkp.webp"
 },
 
 {
@@ -28,7 +28,7 @@ const label = [{
     street : "Krumkmakargatan"
     streetNumber : 22
     zipcode : "11851"
-    logo : 
+    logo : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597295/Artistify/Year0001_otjwyn.webp"
 },
 
 ]
