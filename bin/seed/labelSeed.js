7867311a3c63d45a025c675551eb7985cd13642b
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-03-01 12:11:25 +0100

    with label seed

--- bin/seed/labelSeed.js
+++ bin/seed/labelSeed.js
@@ -1,1 +1,34 @@
 require("../../config/mongo");
+
+const label = [{
+    
+    name : "XL Recordings"
+    city : "London"
+    country : "UK"
+    street : "Codrington Mews, Notting Hill"
+    streetNumber : 1
+    zipcode : "W11 2EH"
+    logo : 
+}, 
+
+{
+    name : "Ghostly International"
+    city : "New York"
+    country : "USA"
+    street : "PO Box"
+    streetNumber : 379
+    zipcode : "10276"
+    logo : 
+},
+
+{
+    name : "YEAR0001"
+    city : "Stockholm"
+    country : "Sweden"
+    street : "Krumkmakargatan"
+    streetNumber : 22
+    zipcode : "11851"
+    logo : 
+},
+
+]
