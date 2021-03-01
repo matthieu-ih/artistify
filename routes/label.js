19451e144ec1af3f5033427b42c54f8e45bcdbea
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-03-01 20:34:16 +0100

    labels update

--- routes/label.js
+++ routes/label.js
@@ -14,4 +14,49 @@ router.get("/", async (req, res) => {
   })
 });
 
+
+router.get('/create', (req, res, next) => {
+  res.render('dashboard/labelCreate');
+});
+
+router.post('/create', (req, res, next) => {
+  LabelModel.create(req.body)
+  .then(newLabel => {
+    console.log(newLabel)
+    res.redirect('/dashboard/label')
+  })
+  .catch((error) => {
+    next(error);
+    res.render('dashboard/labelCreate');
+    console.log(error);
+  })
+})
+
+
+router.get('/update/:id', (req, res, next) => {
+  LabelModel.findById(req.params.id)
+  .then((label)=>{
+    res.render("dashboard/labelUpdate", {label})
+  })
+  .catch((error)=> {
+    next(error);
+    console.log(error)
+
+  })
+});
+
+router.post('/update/:id', (req, res, next) => {
+  LabelModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
+  .then((label)=> {
+    res.redirect("/dashboard/label")
+
+  })
+  .catch((error)=> {
+  next(error);
+  res.render("dashboard/labelUpdate")
+
+  });
+
+});
+
 module.exports = router;
