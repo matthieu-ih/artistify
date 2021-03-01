7cb038379bfa5c6fe28732bf89df3fc9635f9443
Magali <fort_magali@hotmail.fr>
2021-03-01 12:31:16 +0100

    update labelSeeds

--- bin/seed/labelSeed.js
+++ bin/seed/labelSeed.js
@@ -1,34 +1,46 @@
-require("../../config/mongo");
-
 const label = [{
     
-    name : "XL Recordings"
-    city : "London"
-    country : "UK"
-    street : "Codrington Mews, Notting Hill"
-    streetNumber : 1
-    zipcode : "W11 2EH"
+    name : "XL Recordings",
+    city : "London",
+    country : "UK",
+    street : "Codrington Mews, Notting Hill",
+    streetNumber : 1,
+    zipcode : "W11 2EH",
     logo : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597294/Artistify/XL_ladgfw.webp"
 }, 
 
 {
-    name : "Ghostly International"
-    city : "New York"
-    country : "USA"
-    street : "PO Box"
-    streetNumber : 379
-    zipcode : "10276"
+    name : "Ghostly International",
+    city : "New York",
+    country : "USA",
+    street : "PO Box",
+    streetNumber : 379,
+    zipcode : "10276",
     logo : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597295/Artistify/Ghostly_btlkkp.webp"
 },
 
 {
-    name : "YEAR0001"
-    city : "Stockholm"
-    country : "Sweden"
-    street : "Krumkmakargatan"
-    streetNumber : 22
-    zipcode : "11851"
+    name : "YEAR0001",
+    city : "Stockholm",
+    country : "Sweden",
+    street : "Krumkmakargatan",
+    streetNumber : 22,
+    zipcode : "11851",
     logo : "https://res.cloudinary.com/dsldnzayz/image/upload/v1614597295/Artistify/Year0001_otjwyn.webp"
 },
 
 ]
+
+require("../../config/mongo");
+const LabelModel = require("./../../model/Label");
+const mongoose = require("mongoose");
+
+LabelModel.create(label)
+.then((dbsuccess) => {
+    console.log(dbsuccess.length, " labels have been created");
+    mongoose
+      .disconnect()
+      .then((ok) => console.log("mongoDB disconnected"))
+      .catch((err) => console.log("error disconnecting mongoDB ", err));
+  })
+  .catch((dbError) => console.log(dbError));
