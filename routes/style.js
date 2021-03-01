ec76b8cf0d4b38b6b686aee0242c67449d2833de
Magali <fort_magali@hotmail.fr>
2021-03-01 14:00:48 +0100

    add styles view

--- routes/style.js
+++ routes/style.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const StylesModel = require("./../model/Styles");
 
 /* GET home page. */
 router.get("/", async (req, res) => {
-  res.render("dashboard/styles");
+    StylesModel.find()
+    .then((styles)=> {
+    res.render("dashboard/styles", {styles});
+    })
+    .catch((err) => {
+        next(err);
+    })
 });
 
 module.exports = router;
